f552acb47db1d02a6b129e43e8928e900014abeb
Danie Lima <daniellimaf14@gmail.com>
2025-01-29 03:53:37 -0300

    Removendo dist do repositório e adicionando ao .gitignore

--- dist/utils/calcExtractor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*import { Transaction } from "../database/respository";
-
-export function getBalance(statements: Transaction[]) {
-  const balance = statements.reduce((acc, statement) =>
-    statement.type.toLowerCase() === "credit" ? acc + statement.amount : acc - statement.amount, 0);
-  return balance;
-}
-*/ 
